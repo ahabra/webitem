36da7576e995a98cd91c77e883a52d7963d5e12a
Abdul Habra <abdul.habra@gmail.com>
2021-03-16 14:27:08 -0400

    update dependencies

--- scripts/build.utils.js
+++ scripts/build.utils.js
@@ -7,12 +7,15 @@ const Print = require('./console.utils').Print
 const target = 'target'
 const out = `${target}/out`
 const dist = 'dist'
-const banner = `// webitem.js Library to simplify creating HTML5 Custom Elements
+const banner = {
+  js: `// webitem.js Library to simplify creating HTML5 Custom Elements
 // https://github.com/ahabra/webitem
 // Copyright 2021 (C) Abdul Habra. Version ${process.env.npm_package_version}.
 // Apache License Version 2.0
 
 `
+}
+
 
 function clean() {
   fs.rmSync(target, { recursive: true, force: true})
